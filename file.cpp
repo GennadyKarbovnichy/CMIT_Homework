44a1304f5de3e7a0178c8f5f845cdf623f909e5e
GennadyKarbovnichy <82757101+GennadyKarbovnichy@users.noreply.github.com>
2021-04-24 16:54:09 +0400

    removed figure brackets around count_if

--- file.cpp
+++ file.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
-    cout << {count_if(nums.begin(), nums.end(),
+    cout << count_if(nums.begin(), nums.end(),
         [](int num) {
             return (num > 2);
-        })};
+        });
 }
