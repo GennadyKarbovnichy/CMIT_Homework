aebab8ea802a80d87700352bd715954ccee10013
GennadyKarbovnichy <82757101+GennadyKarbovnichy@users.noreply.github.com>
2021-08-29 14:54:17 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
-#include <QCoreApplication>
-#include <domessagerserver.h>
-
-using namespace cmit;
-
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-    doMessagerServer server;
-    if(!server.startserver(3333)) {
-        qDebug() << "Error: " << server.errorString();
-        return 1;
-    }
-    qDebug() << "Server started...";
-    return a.exec();
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello world";
+    return 0;
 }
