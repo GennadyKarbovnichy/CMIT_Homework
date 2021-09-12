b47ff5b205f2b52d6558ab2876d008421a259876
GennadyKarbovnichy <82757101+GennadyKarbovnichy@users.noreply.github.com>
2021-09-12 14:31:07 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,21 +8,23 @@ struct Order {
 	int id;
 	string description;
 	string status;
-	void print() {
-		cout << "ID: " << id <<
-			"\nDescription: " << description <<
-			"\nStatus: " << status << endl;
-	}
 };
 
-void orderAdd(vector<Order>& vect);
+void orderPrint(const Order& ord);
+
+void orderAdd(vector<Order>& vect, int ordersDeleted);
 
 void orderPrintAll(const vector<Order>& vect);
 
-void orderFind(const vector<Order>& vect, const int& orderid);
+int orderFind(const vector<Order>& vect, const int& orderid);
+
+void orderEdit(vector<Order>& vect, const int& orderid);
+
+void orderDelete(vector<Order>& vect, const int& orderid, int& ordersDeleted);
 
 int main() {
 	int a;
+	int ordersDeleted = 0;
 	vector<Order> orders;
 	bool key = true;
 	while(key) {
@@ -32,12 +34,14 @@ int main() {
 			"\t1) Add an order\n" <<
 			"\t2) Print all orders\n" <<
 			"\t3) Find the order\n" <<
-			"\t4) Exit\n";
+			"\t4) Edit the order\n" <<
+			"\t5) Delete the order\n" <<
+			"\t6) Exit\n";
 		cin >> a;
 		
 		switch(a) {
 			case 1: {
-				orderAdd(orders);
+				orderAdd(orders, ordersDeleted);
 				break;
 			}
 			case 2: {
@@ -48,10 +52,49 @@ int main() {
 				int orderid;
 				cout << "What is ID of this order? ";
 				cin >> orderid;
-				orderFind(orders, orderid);
+				orderid = orderFind(orders, orderid);
+				if (orderid != 0) {
+					cout << "We found it! Look:\n";
+					orderPrint(orders[orderid - ordersDeleted - 1]);
+					system("pause");
+				}
+				else {
+					cout << "Order isn't found. Please try again.\n";
+					system("pause");
+				}
 				break;
 			}
 			case 4: {
+				int orderid;
+				cout << "What is ID of this order? ";
+				cin >> orderid;
+				orderid = orderFind(orders, orderid);
+				if (orderid != 0) {
+					orderEdit(orders, orderid);
+					system("pause");
+				}
+				else {
+					cout << "Order isn't found. Please try again.\n";
+					system("pause");
+				}
+				break;
+			}
+			case 5: {
+				int orderid;
+				cout << "What is ID of this order? ";
+				cin >> orderid;
+				orderid = orderFind(orders, orderid);
+				if (orderid != 0) {
+					orderDelete(orders, orderid, ordersDeleted);
+					system("pause");
+				}
+				else {
+					cout << "Order isn't found. Please try again.\n";
+					system("pause");
+				}
+				break;
+			}
+			case 6: {
 				key = false;
 				break;
 			}
